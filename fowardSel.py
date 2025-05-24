094f3a9d32e0b15486721d94dd1616d123fc8aaa
Dekamayaro <dekamayaro@gmail.com>
2025-05-24 13:13:25 -0700

    started building the structure of queue for our problem

--- fowardSel.py
+++ fowardSel.py
@@ -1,5 +1,6 @@
 import node
 import numpy as np
+from queue import PriorityQueue
 
 
 def eval(Node:node):
@@ -10,17 +11,25 @@ def forwardSel(fileName):
 
     #finding number of features, columns in data:
     numFeatures = data.shape[1]
+    featureQueue = PriorityQueue()
 
     print(f"Using no features and “random” evaluation, I get an accuracy of\n")
-    
+
     #find begin acc:
-    beginAcc = None #eval() #! need to include start node
+    beginAcc = eval(node()) #! need to include start node
     print(f"{beginAcc}% Beginning search.")
 
     for feat in numFeatures:
         #find accuracy of selected feature(s):
+        currNode = node() #!add features to node --> still need to think of how though.
+
         featureAcc = None #!eval function here 
         print(f"Using feature(s) {feat} accuracy is {featureAcc}%\n")
         #!need to create a node for it and set the accuracy
+
     
     #need to find the best feature now:
+        #!we can use a maxheap with accuracy 
+         # maybe add to before we find the accuracy and then 
+
+
--- node.py
+++ node.py
@@ -4,7 +4,7 @@ class node:
     features = []
     accuracy = 0
 
-    def __init__(self, features, accuracy = 0):
+    def __init__(self, features = [], accuracy = 0):
         self.features = features
         self.accuracy = accuracy
         
