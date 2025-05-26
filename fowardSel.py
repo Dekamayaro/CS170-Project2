857605dffdba733dcb1405901520f59bdb2923ec
KyleChahal <chahalkyle@gmail.com>
2025-05-26 03:39:51 -0700

    I think i finished forwardSel, but will need testing

--- fowardSel.py
+++ fowardSel.py
@@ -4,7 +4,7 @@ from queue import PriorityQueue
 
 
 def eval(Node:node):
-    pass
+    return round(random.uniform(35.0, 95.0), 2)
 
 def forwardSel(fileName):
     data = np.loadtxt(fileName)
@@ -14,8 +14,10 @@ def forwardSel(fileName):
 
     #finding number of features, columns in data:
     numFeatures = data.shape[1]
+    remainingFeat = list(range(1, numFeatures + 1))
     featureQueue = PriorityQueue()
-
+    labels = data[:, 0]
+    features = data[:, 1:]
     print(f"Using no features and “random” evaluation, I get an accuracy of\n")
 
     #find begin acc:
@@ -23,11 +25,12 @@ def forwardSel(fileName):
     print(f"{beginAcc}% Beginning search.")
 
     while(False == isStop):
-
-        for feat in numFeatures:
+        currBest = None
+        for feat in remainingFeat:
             #find accuracy of selected feature(s):
-            currNode = node() #!add features to node --> still need to think of how though.
-
+            #currNode = node() #!add features to node --> still need to think of how though.
+            currFeatures = selectedFeat + [feat]
+            currNode = node(features = currFeatures)
             currNode.setAccuracy(eval(currNode))
             featureQueue.put(currNode)
             print(f"Using feature(s) {feat} accuracy is {currNode.accuracy}%\n")
@@ -43,7 +46,13 @@ def forwardSel(fileName):
         else:
             maxAcc = currBest.accuracy
             print(f"Feature set {currBest.features} was best, accuracy is {maxAcc}%\n")
-            selectedFeat.append[currBest.features] #! not foolproof, will add repeat nums 
+            selectedFeat = currBest.features
+            tempRemainingFeat = []
+            for i in selectedFeat:
+                if i != feat:
+                    tempRemainingFeat.append(i)
+            remainingFeat = tempRemainingFeat
+            #! not foolproof, will add repeat nums 
             #! also need to update numfeatures --> remove the feature chosen last 
         
 
