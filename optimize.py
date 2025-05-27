9b07aef530f1bb1a78adc8a200d75f0bd369b85c
Dekamayaro <dekamayaro@gmail.com>
2025-05-26 18:43:32 -0700

    allegedly finished backwards elimination

--- optimize.py
+++ optimize.py
@@ -1,6 +1,7 @@
 import node
 import numpy as np
 from queue import PriorityQueue
+import random
 
 
 def eval(Node:node):
@@ -68,46 +69,18 @@ def backwardsElim(fileName):
     beginAcc = eval(node(list(range(1,numFeatures)))) #! need to include start node
     print(f"{beginAcc}% Beginning search.")
 
-    remainingFeat = list(range(1, numFeatures + 1))
+    selectedFeat = remainingFeat = list(range(1, numFeatures + 1))
     featureQueue = PriorityQueue()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-
-    remainingFeat = list(range(1, numFeatures + 1))
-    featureQueue = PriorityQueue()
-    labels = data[:, 0]
-    features = data[:, 1:]
-    print(f"Using no features and “random” evaluation, I get an accuracy of\n")
-
-    #find begin acc:
-    beginAcc = eval(node()) #! need to include start node
-    print(f"{beginAcc}% Beginning search.")
 
     while(False == isStop):
         currBest = None
+
         for feat in remainingFeat:
-            #find accuracy of selected feature(s):
-            #currNode = node() #!add features to node --> still need to think of how though.
-            currFeatures = selectedFeat + [feat]
+            currFeatures = selectedFeat - [feat]
             currNode = node(features = currFeatures)
             currNode.setAccuracy(eval(currNode))
             featureQueue.put(currNode)
-            print(f"Using feature(s) {feat} accuracy is {currNode.accuracy}%\n")
-            
+            print(f"Using feature(s) {feat} accuracy is {currNode.accuracy}%\n")            
         
         #need to find the best feature now:
         currBest = featureQueue.get()
@@ -119,12 +92,13 @@ def backwardsElim(fileName):
         else:
             maxAcc = currBest.accuracy
             print(f"Feature set {currBest.features} was best, accuracy is {maxAcc}%\n")
-            selectedFeat = currBest.features
-            tempRemainingFeat = []
-            for i in selectedFeat:
-                if i != feat:
-                    tempRemainingFeat.append(i)
-            remainingFeat = tempRemainingFeat
+            remainingFeat = selectedFeat = currBest.features
+            #tempRemainingFeat = []
+
+            # for i in selectedFeat:
+            #     if i != feat:
+            #         tempRemainingFeat.append(i)
+            # remainingFeat = tempRemainingFeat
             #! not foolproof, will add repeat nums 
             #! also need to update numfeatures --> remove the feature chosen last 
 
