c2e7500a35e91a5d7cd2b220d6cb3108bdeb8b33
Dekamayaro <dekamayaro@gmail.com>
2025-05-31 13:44:16 -0700

    started on part3, need to fix forward select, have not touched backwards yet.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import node
 import validator
 import classifier
 import part1
+import part3
 
 def main():
     print("Welcome to Dmitry Sorokin and Kyle Chahal's Feature Selection Algorithm")
@@ -21,7 +22,31 @@ def main():
     else:
         print("Invalid Option")
         
-        
+def mainP3():
+    print("Welcome to Dmitry Sorokin and Kyle Chahal's Feature Selection Algorithm\n\n")
 
-main()
+    print(f"Here is a list of avaliable dataasets:\n\t1) Small-Test-Dataset\n\t2) Large-Test-Dataset")
+    dataset = int(input("Please choose the number of the dataset you would like to use:"))
+    if(1 == dataset):
+        filename = "small-test-dataset.txt"
+    elif(2 == dataset):
+        filename = "large-test-dataset.txt"
+
+    print("\n Type the number of the algorithm you want to run")
+    print("1) Forward Selection")
+    print("2) Backward Elimination")
+    
+    user_choice = int(input())
+    
+    if user_choice == 1:
+        part3.forwardSel(filename)
+    elif user_choice == 2:
+        pass
+        #part3.backwardsElim(filename)
+    else:
+        print("Invalid Option")
+
+
+#main()
+mainP3()
 
